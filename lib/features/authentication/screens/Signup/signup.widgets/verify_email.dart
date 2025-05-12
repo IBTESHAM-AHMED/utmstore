9d97a4682f62dd6fa86b9b62623a85fcc6fad25f
IBTESHAM-AHMED <117981993+IBTESHAM-AHMED@users.noreply.github.com>
2025-05-12 21:02:39 +0800

    verify email

---
+++ lib/features/authentication/screens/Signup/signup.widgets/verify_email.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constant/sizes.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (), icon: icon)
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              ///Image
+              
+              ///Title& Subtitle
+
+              ///Buttons
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
